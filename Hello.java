0a4fafb44780c97e9588fde2f3ccde541bafe09a
ofirlevy1 <ofirl4585@gmail.com>
2022-11-16 19:49:38 +0200

    a

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
-﻿public class Hello
+public class Hello
 {
 	public static void main(String[] args)
 	{
-		for(int i=1;i<=10;i++)
-			System.out.println("Hello World..."+i);
+		for(int i=1; i<=10; i++)
+			System.out.println("Hello world..." + i);
 	}
 }
